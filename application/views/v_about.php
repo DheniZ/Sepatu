fc154cfb3e41df892d7e0174e9a48eb41398ae05
DheniZ <12200335@bsi.ac.id>
2021-10-16 11:43:06 +0700

    initial commit

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Dheni</li>
+        <li>Nama Belakang : Zaelani</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Wall Maria </li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Wall Rose</li>
+    </ul>
+    <h4>Lagu Favorit</h4>
+    <ul type="square">
+        <li>Bring Me The Horizon - Mother Tongue</li>
+    </ul>
+</section>
